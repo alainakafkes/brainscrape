de89efde2d23cc99ce5270e74904c8545cb57e2f
Alaina Kafkes <alainakafkes2017@u.northwestern.edu>
2016-07-30 19:44:42 -0500

    Added search by author quote scraper

--- brainscrape.py
+++ brainscrape.py
@@ -1,10 +1,12 @@
 # BrainyQuote Web Scraper (By Keyword)
+# SPECIAL POMMUNISM EDITION
 # Alaina Kafkes
 
 import requests
 from bs4 import BeautifulSoup
 
 example_key = "lemons"
+example_author = "kurt_vonnegut"
 
 def getQuotes(keyword=example_key, numpages=7):
     """
@@ -36,4 +38,36 @@ def getQuotes(keyword=example_key, numpages=7):
 
     # Create list of tuples of the form (quote, author)
     ans = zip(quoteArray, authorArray)
-    return ans
+    return ans, len(ans)
+
+def getQuotesByAuthor(author=example_author, numpages=4):
+    """
+    Given the author name and the number of HTML pages of quotes to parse, uses Requests & BeautifulSoup to obtain (quote, author) tuples from BrainyQuote.
+    Returns list of (quote, author) tuples and the length of this list.
+    """
+    # Initialize lists
+    quoteArray = []
+    authorArray = []
+    pageNameArray = [author]
+    for i in range(2,numpages+1):
+        pageNameArray.append(author + "_" + str(i))
+
+    # For every page pertaining to a topic
+    for page in pageNameArray:
+        # Obtain BrainyQuote page html
+        base_url = "http://www.brainyquote.com/quotes/authors/"
+        url = base_url + author[0] + "/" + author + ".html"
+        response_data = requests.get(url).text[:]
+        soup = BeautifulSoup(response_data, 'html.parser')
+
+        # Populate quoteArray
+        for item in soup.find_all("span", class_="bqQuoteLink"):
+            quoteArray.append(item.get_text().rstrip())
+
+        # Populate authorArray
+        for item in soup.find_all("div", class_="bq-aut"):
+            authorArray.append(item.get_text())
+
+    # Create list of tuples of the form (quote, author)
+    ans = zip(quoteArray, authorArray)
+    return ans, len(ans)
